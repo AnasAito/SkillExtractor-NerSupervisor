e4ed12d34315fa11ff53e5d5cd6b2e79cd98200f
Anas AIT AOMAR <56308112+AnasAito@users.noreply.github.com>
2021-08-07 02:30:20 +0100

    fix header

--- src/header.js
+++ src/header.js
@@ -1,10 +1,39 @@
 /* This example requires Tailwind CSS v2.0+ */
+const pages = [
+  { name: "Job profiles", href: "#", current: false },
+  { name: "1_1", href: "#", current: true },
+  { name: "1", href: "#", current: true }
+];
 export default function Example() {
   return (
     <div className="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
-      <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Job descriptions 1/19
-      </h3>
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol className="flex items-center space-x-4">
+          <p className="font-bold ">NER SUPERVISOR</p>
+          {pages.map((page) => (
+            <li key={page.name}>
+              <div className="flex items-center">
+                <svg
+                  className="flex-shrink-0 h-5 w-5 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
+                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                </svg>
+                <a
+                  href={page.href}
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  aria-current={page.current ? "page" : undefined}
+                >
+                  {page.name}
+                </a>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </nav>
       <div className="mt-3 flex sm:mt-0 sm:ml-4">
         <button
           type="button"
